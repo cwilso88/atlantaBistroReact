58be060922e90306b3a4c796a8516ea4f508e537
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-08 21:09:49 -0400

    added alt to img in reviews component

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
 
 class Reviews extends Component {
+    state = {
+        currentReview: 0
+    }
+    
+    leftClick = (state, props) => {
+        if(state.globalState.currentReview == 0 ) {
+          console.log('do nothing')
+        } else {
+          reviewLeft();
+        }
+      }
+    
+      rightClick = (state, props) => {
+        if(state.globalState.currentReview == (reviewIndex - 1)) {
+          console.log('do nothing')
+        } 
+      }
+
+      currentReviewDisplay = (state, actions) => {
+        return (
+          <div>
+            <h5 className="reviews-title">Reviews</h5>
+            <h2>{reviewInfo[currentReview].company}</h2>
+            <h4>{reviewInfo[currentReview].highlight}</h4>
+            <p>{reviewInfo[currentReview].review}</p>
+          
+            <div className="author"><strong>{reviewInfo[currentReview].author}</strong>- <em>{reviewInfo[currentReview].authorInfo}</em></div>
+          </div>
+        )
+      }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <section id="Reviews">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 side-img">
+                            <img src="https://image.freepik.com/free-photo/chef-with-his-arms-crossed-over-white-background_1368-2792.jpg" alt="Master Chef"/>
+                        </div>
+                        <div className="col-md-4">
+                            {this.currentReviewDisplay()}
+                            <div className="arrows">
+                                <i onclick={() => console.log(state.globalState.currentReview - 1)} class={`fa fa-arrow-left ${(currentReview > 0) ? 'ready' : ''}`}></i>
+                                <i onclick={() => console.log(state.globalState.currentReview + 1)} class={`fa fa-arrow-right ${(currentReview == (reviewIndex - 1)) ? '' : 'ready'}`}></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
---
+++ src/data/reviewsData.js
@@ -0,0 +1,37 @@
+export const reviewsInfo = [
+    {
+      company: 'The Food Network',
+      review: 'The Atlanta Bistro exemplifies amazing service and good quality. We inspected the quality of the menu and the cleanliness of the kitchen. The chef seems to make sure that the dishes offered are of the highest quality. The dining area is sleek and modern, perfect for the Atlanta scene.',
+      author: 'Joe Bastiachi',
+      authorInfo: 'Winner Of The Chef Masterclass',
+      highlight: '"Best Restaurant in the Atlanta area!"'
+    },
+    {
+      company: 'Gayot',
+      review: 'The Atlanta Bistro exemplifies amazing service and good quality. We inspected the quality of the menu and the cleanliness of the kitchen. The chef seems to make sure that the dishes offered are of the highest quality. The dining area is sleek and modern, perfect for the Atlanta scene.',
+      author: 'Robert Sietsema',
+      authorInfo: 'Winner Of The Chef Masterclass',
+      highlight: '"High quality food and modern decor!"'
+    },
+    {
+      company: 'Roadfood',
+      review: 'The Atlanta Bistro exemplifies amazing service and good quality. We inspected the quality of the menu and the cleanliness of the kitchen. The chef seems to make sure that the dishes offered are of the highest quality. The dining area is sleek and modern, perfect for the Atlanta scene.',
+      author: 'Josh Ozersky',
+      authorInfo: 'Winner Of The Chef Masterclass',
+      highlight: '"Top notch service and food!"'
+    },
+    {
+      company: 'BlackboardEats',
+      review: 'The Atlanta Bistro exemplifies amazing service and good quality. We inspected the quality of the menu and the cleanliness of the kitchen. The chef seems to make sure that the dishes offered are of the highest quality. The dining area is sleek and modern, perfect for the Atlanta scene.',
+      author: 'Kenji Lopez-Alt',
+      authorInfo: 'Winner Of The Chef Masterclass',
+      highlight: '"Luxury Bistro dining with a twist!"'
+    },
+    {
+      company: 'HBO',
+      review: 'The Atlanta Bistro exemplifies amazing service and good quality. We inspected the quality of the menu and the cleanliness of the kitchen. The chef seems to make sure that the dishes offered are of the highest quality. The dining area is sleek and modern, perfect for the Atlanta scene.',
+      author: 'Ben Leventhal',
+      authorInfo: 'Winner Of Atlanta Best Chef',
+      highlight: '"Amazing Dining experience for all!"'
+    },
+  ]
