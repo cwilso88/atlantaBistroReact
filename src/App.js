100cada94073d6097a86e09adfa2268155156260
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-09 10:24:08 -0400

    Updated the scss for the OurStory component to fix styling bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import "./sass/main.scss";
+
 import Header from './components/Header';
 import Topimg from './components/Topimg';
 import ContactUs from './components/ContactUs';
@@ -9,8 +12,9 @@ import OurStory from './components/OurStory';
 import RandomQuote from './components/RandomQuote';
 import SpecialMenu from './components/SpecialMenu';
 import Reviews from './components/Reviews';
+import Footer from './components/Footer';
 
-import "./sass/main.scss";
+
 
 function App() {
   return (
@@ -22,6 +26,7 @@ function App() {
         <SpecialMenu />
         <Reviews props={reviewsData} />
         <ContactUs />
+        <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <footer>
+      <div className="container">
+          <nav className="menu">
+            <a href="#OurStory">Our Story</a>
+            <a href="#SpecialMenu">Special Menu</a>
+            <a href="#Reviews">Reviews</a>
+            <a href="#ContactUs">Contact Us</a>
+          </nav>
+
+          <ul className="social-media">
+              <li>
+                <a href="http://www.facebook.com" target="new">
+                  <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.twitter.com" target="new">
+                  <i className="fa fa-twitter-square" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://instagram.com" target="new">
+                  <i className="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://pintrest.com" target="new">
+                  <i className="fa fa-pinterest-square" aria-hidden="true"></i>
+                </a>
+              </li>
+          </ul>
+
+          <div className="copyright">
+          © 2019 Copyright <br/> This site was happily made by Chasity De La Cruz
+          </div>
+          
         </div>
+    </footer>
     )
 }
 
